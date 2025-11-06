65f179f10619e2d44fd3fea2bab18cec1edd61da
Ayush B <bhaleraoayush06@gmail.com>
2025-11-06 20:07:00 +0530

    added directory wathcing need to add cliarguments

--- scripts/script.py
+++ scripts/script.py
@@ -3,7 +3,15 @@ import os
 from process_file import process_file, files_dir 
 import time
 
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+
+
 MAX_THREADS = 6  # max concurrent threads
+
+
 
 def main():
     files = [
@@ -35,5 +43,27 @@ def main():
 
     print(f"\n All files processed in {time.time() - start_time:.2f}s\n")
 
+class Watcher(FileSystemEventHandler):
+    def on_created(self, event):
+        # event.src_path is the full path of the new file
+        if event.is_directory:
+            return
+        file_path = event.src_path
+        print(f"[+] New file detected: {os.path.basename(file_path)}")
+        try:
+            main(file_path)
+        except Exception as e:
+            print(f"[!] Error processing {file_path}: {e}")
+
 if __name__ == "__main__":
-    main()
+    print(f"👀 Watching folder: {os.path.abspath(files_dir)}")
+    event_handler = Watcher()
+    observer = Observer()
+    observer.schedule(event_handler, files_dir, recursive=False)
+    observer.start()
+    try:
+        while True:
+            time.sleep(2)  # keeps the program running
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
